8d09e03453a6ebf64eb6c2b59db91c0d4031c326
hsz <santinogz131@gmail.com>
2024-09-19 19:46:53 -0300

    Actualizacion de dependencias, eliminacion de plantillas weather, cambios minimos en el codigo

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Solution_IBIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _service;
+        public UserController(UserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok(_service.Get(name));
+        }
+    }
+}
